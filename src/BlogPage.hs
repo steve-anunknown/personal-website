677f4fd80e194c31c17066b9dcc353ea5451cfd3
Stefanos Anagnostou <steve.anunknown@gmail.com>
2024-12-16 20:49:02 +0200

    restore blog page to original state

--- src/BlogPage.hs
+++ src/BlogPage.hs
@@ -1,19 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module BlogPage (blogPage) where
 
 import RouteMaker (mkRoute)
-import ComparingLogMessages (comparingLogMessages)
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 import Web.Scotty as S
 
-
 blogPage :: ScottyM ()
 blogPage = mkRoute "/blogs" "My Blogs" "logo2.svg" $ do
-  H.div ! A.class_ "blog-page" $ do
-    H.h1 "My Blogs"
-    comparingLogMessages
-    H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "/css/style.css"
-    H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "/css/blog-style.css"
+  H.h1 "My Blogs"
+  H.p "Turns out I'm not that interesting."
+  H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "/css/style.css"
